e4a8069e40c03921775bf5d78b032923d404515b
js <1208175466@qq.com>
2023-05-22 20:47:51 +0800

    finally

--- app.py
+++ app.py
@@ -16,32 +16,32 @@ app = Flask(__name__)
 # 定义路由
 @app.route('/', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
+    if request.method == 'POST':  #post是一种请求方式
         # 从表单中获取上传的文件
-        f = request.files['file']
-        global filename
-        filename = f.filename
-        global file_path 
+        f = request.files['file']  #request.files 函数作用就是获取前端名为 'file'的文件信息
+        global filename  # 定义全局变量，方便其他地方调用filename，如果不定义全局变量，其他地方无法调用
+        filename = f.filename  # 获取前端上传图片名字
+        global file_path  #同理，定义全局变量
         
         # 将文件保存到服务器本地
-        file_path = os.path.join(os.getcwd(), filename)
+        file_path = os.path.join(os.getcwd(), filename)  #本地路径+图片名字= 文件路径（file-path)
 
-        print(file_path)
-        f.save(file_path)
+        print(file_path)  # 当时只是为了测试程序
+        f.save(file_path)  # 保存上传的图片到本地目录下，方便后续推理，直接找到图片
         # 返回文件路径
         # return file_path
-    return render_template('index1.html')
+
+        #进行检测
+        opt = parse_opt() 
+        main(opt)
+    return render_template('index.html')
 
 #检测函数
-@app.route('/det', methods=['GET','POST'])
-def my_flask_function():
-    #print('测试一下！')
-    opt = parse_opt()
-    main(opt)
-
-
-    # return jsonify({'message': 'Hello from Flask!'})
-    return render_template('123.html')
+# @app.route('/det', methods=['GET','POST'])
+# def my_flask_function():
+#     #print('测试一下！')
+#     # return jsonify({'message': 'Hello from Flask!'})
+#     return render_template('123.html')
 
 # 检测结果显示
 def return_img_stream(img_local_path):
@@ -58,11 +58,12 @@ def return_img_stream(img_local_path):
         img_stream = base64.b64encode(img_stream).decode()
     return img_stream
 
-@app.route('/sh', methods=['GET', 'POST'])
+@app.route('/sh', methods=['GET', 'POST'])  #定义新路由，显示图片
 def hello_world():
-    img_path = 'runs\\detect\\exp\\' + str(filename)
-    img_stream = return_img_stream(img_path)
-    return render_template('showimage.html', img_stream=img_stream)
+    #图片路径，推理完之后，默认保存的就是runs\\detect\\exp，这里加上filename，是变成完整的图片路径，然后才能获取显示
+    img_path = 'runs\\detect\\exp\\' + str(filename)  
+    img_stream = return_img_stream(img_path) #获取图片流
+    return render_template('index.html', img_stream=img_stream)
 
 # 检测图片的
 
@@ -91,4 +92,7 @@ def parse_opt():
 
 # 启动Flask应用
 if __name__ == '__main__':
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.run(host='0.0.0.0', port=5000)
+    
+
